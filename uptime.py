4389a05d7f7154ed1c37568afff1a171a8db2c53
Topscientist <74202229+Topscientist@users.noreply.github.com>
2023-08-02 21:43:32 +0000

    Changed the message of the uptime website

--- uptime.py
+++ uptime.py
@@ -5,7 +5,7 @@ app = Flask('Artys Moderation Uptime')
 
 @app.route('/')
 def home():
-    return "Heyo! I am alive, wheeee"
+    return "Heyo! You found the secret sauce, my uptime website! There's nothing much here at the moment, but congrats for finding me."
 
 def run():
   app.run(host='0.0.0.0',port=8080)
